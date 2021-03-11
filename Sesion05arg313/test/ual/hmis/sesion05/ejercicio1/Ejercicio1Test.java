092c6b350e7983bca3db6b9a370208d3d4413099
Alejandro Roca <arg313@inlumine.ual.es>
2021-03-11 21:42:09 +0100

    Pruebas con JUnit

--- Sesion05arg313/test/ual/hmis/sesion05/ejercicio1/Ejercicio1Test.java
+++ Sesion05arg313/test/ual/hmis/sesion05/ejercicio1/Ejercicio1Test.java
@@ -1,5 +1,23 @@
 package ual.hmis.sesion05.ejercicio1;
 
-public class Ejercicio1Test {
+import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class Ejercicio1Test {
+	
+	@ParameterizedTest (name = "{index} => Con entrada ({0}) sale {1}")
+	@CsvSource({"2, 1",
+				"10, 4",
+				"6, 2",
+				"14, 7"})
+	
+	void testTransformar(int input, int expected) {
+		Ejercicio1 c = new Ejercicio1();
+		assertEquals(expected, c.transformar(input));
+		// assertTrue(c.transformar(input)==expected);
+	}
 }
