71da527b9ca8d47cbad731ce53ae92ce695fce6a
Alejandro Roca <arg313@inlumine.ual.es>
2021-03-18 13:18:32 +0100

    Cambio a Maven

--- Sesion05arg313/test/ual/hmis/sesion05/ejercicio1/Ejercicio1Test.java
+++ Sesion05arg313/test/ual/hmis/sesion05/ejercicio1/Ejercicio1Test.java
@@ -8,13 +8,10 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class Ejercicio1Test {
-	
-	@ParameterizedTest (name = "{index} => Con entrada ({0}) sale {1}")
-	@CsvSource({"2, 1",
-				"10, 4",
-				"6, 2",
-				"14, 7"})
-	
+
+	@ParameterizedTest(name = "{index} => Con entrada ({0}) sale {1}")
+	@CsvSource({ "2, 1", "10, 4", "6, 2", "14, 7" })
+
 	void testTransformar(int input, int expected) {
 		Ejercicio1 c = new Ejercicio1();
 		assertEquals(expected, c.transformar(input));
